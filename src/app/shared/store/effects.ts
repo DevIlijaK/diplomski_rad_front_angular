27f5c5e24969e6baac769624f1f951dfd0bb5f14
ilija12354 <ilijakoske@gmail.com>
2022-07-30 21:20:17 +0200

    'idk'

--- src/app/shared/store/effects.ts
+++ src/app/shared/store/effects.ts
@@ -2,9 +2,6 @@ import {Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {Router} from '@angular/router';
 import {map, switchMap, withLatestFrom} from 'rxjs/operators';
-import {ToastrService} from 'ngx-toastr';
-import {TranslateService} from '@ngx-translate/core';
-import {MatDialog} from '@angular/material/dialog';
 import {of} from 'rxjs';
 import * as SharedActions from './actions';
 import {
@@ -26,9 +23,6 @@ import {ROUTER_NAVIGATION, RouterNavigationAction} from '@ngrx/router-store';
 @Injectable()
 export class SharedEffects {
   constructor(private action$: Actions, private navigator: Router,
-              private notificationMessages: ToastrService,
-              private translateService: TranslateService,
-              private dialog: MatDialog,
               private store$: Store<AppState>,
               private spinnerService: NgxSpinnerService
   ) {
@@ -43,37 +37,37 @@ export class SharedEffects {
     ), {dispatch: false}
   );
 
-  openSuccessDialog = createEffect(
-    () => this.action$.pipe(
-      ofType(successMessages),
-      map((data) => {
-        let messages = this.translateService.instant(data.messagesKey);
-        messages = data.extraMessage ? messages + ' - ' + data.extraMessage : messages;
-        this.notificationMessages.success(messages);
-      }),
-    ), {dispatch: false},
-  );
+  // openSuccessDialog = createEffect(
+  //   () => this.action$.pipe(
+  //     ofType(successMessages),
+  //     map((data) => {
+  //       let messages = this.translateService.instant(data.messagesKey);
+  //       messages = data.extraMessage ? messages + ' - ' + data.extraMessage : messages;
+  //       this.notificationMessages.success(messages);
+  //     }),
+  //   ), {dispatch: false},
+  // );
 
-  openErrorDialog = createEffect(
-    () => this.action$.pipe(
-      ofType(errorMessages),
-      map((data) => {
-        let messages = this.translateService.instant(data.messagesKey);
-        messages = data.extraMessage ? messages + ' - ' + data.extraMessage : messages;
-        this.notificationMessages.error(messages);
-      }),
-    ), {dispatch: false},
-  );
+  // openErrorDialog = createEffect(
+  //   () => this.action$.pipe(
+  //     ofType(errorMessages),
+  //     map((data) => {
+  //       let messages = this.translateService.instant(data.messagesKey);
+  //       messages = data.extraMessage ? messages + ' - ' + data.extraMessage : messages;
+  //       this.notificationMessages.error(messages);
+  //     }),
+  //   ), {dispatch: false},
+  // );
 
-  openDialog$ = createEffect(
-    () => this.action$.pipe(
-      ofType(openModal),
-      map((data) => {
-        this.dialog.open(data.component, data.config);
-        // return of(SharedActions.openModalSuccess({dialogRef}));
-      }),
-    ),
-    {dispatch: false});
+  // openDialog$ = createEffect(
+  //   () => this.action$.pipe(
+  //     ofType(openModal),
+  //     map((data) => {
+  //       this.dialog.open(data.component, data.config);
+  //       // return of(SharedActions.openModalSuccess({dialogRef}));
+  //     }),
+  //   ),
+  //   {dispatch: false});
 
   closeDialog$ = createEffect(
     () => this.action$.pipe(
